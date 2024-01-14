9d2aa10533192e0ac25e22e994463b68e8ec2f34
Kenny <kenny@qutebox.com>
2024-01-14 15:13:18 +0800

    Fix Nextjs env variable on AppRunner.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_URL: process.env.APP_URL,
+    NEXT_PUBLIC_NAME: process.env.APP_NAME,
+    NEXT_PUBLIC_DEBUG: process.env.APP_DEBUG,
+    NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API,
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
